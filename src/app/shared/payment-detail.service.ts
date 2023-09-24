3db0b990540cb7b0b5c6cba513378bae59a886d8
qvixnh <nguyenvinh22012002@gmail.com>
2023-09-24 20:30:27 +0700

    payment-detail update

--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { PaymentDetail } from './payment-detail.model';
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
   url:string =environment.apiBaseUrl + '/PaymentDetail';
+  list:PaymentDetail[] =[];
+  formData:PaymentDetail = new PaymentDetail();
+
+
   constructor(private http: HttpClient) { }
+
   refreshList(){
     this.http.get(this.url)
     .subscribe({
       next: res =>{
-        console.log(res);
+        this.list = res as PaymentDetail[];
       },
       error: err =>{console.log(err);}
     })
